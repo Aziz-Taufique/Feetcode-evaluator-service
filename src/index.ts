5226e52af3e2e8d4d3bfa4f2b6459b50f9d8e7f2
Aziz-Taufique <taufiqueaziz88@gmail.com>
2026-02-06 15:32:58 +0530

    ping controller added and modify the setup

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-import express, { Express, Request, Response } from "express";
+import express, { Express } from "express";
+
 import serverConfig from "./config/serverConfig";
+import apiRouter from "./routes";
 
 const app: Express = express();
 
-app.get("/", (req: Request, res: Response) => {
-    res.send({ foo: "fsadsas" })
-});
+app.use("/api", apiRouter);
 
 app.listen(serverConfig.PORT, () => {
     console.log(`Server is runnig at port ${serverConfig.PORT}`);
